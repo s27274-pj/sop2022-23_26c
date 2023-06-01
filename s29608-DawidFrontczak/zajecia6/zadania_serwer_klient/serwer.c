77fba306d3b14200750bcec626f20d1ba7846df4
s29608-pj <dawid.frontczak11@gmail.com>
2023-06-01 16:33:02 +0200

    Update serwer.c

--- s29608-DawidFrontczak/zajecia6/zadania_serwer_klient/serwer.c
+++ s29608-DawidFrontczak/zajecia6/zadania_serwer_klient/serwer.c
@@ -14,19 +14,26 @@
 void write_file(int socket_serv){
   int m;
   FILE *plik;
-  char *nazwa_pliku = "plikx.txt";
+  char *nazwa_pliku = "write.txt";
   char buffer[SIZE];
-  plik = fopen(nazwa_pliku, "w");
+  plik = fopen(nazwa_pliku, "wb");
+  
+  if(plik==NULL){
+    int file_size = -1;
+    send(socket_serv, &file_size, sizeof(file_size), 0);
+    printf("BLAD otwierania pliku.\n");
+    exit(1);
+  }
 	
   while (1) {
     m = recv(socket_serv, buffer, SIZE, 0);
     if (m <= 0){
       break;
-      return;
     }
-    fprintf(plik, "%s", buffer);
+    fwrite(buffer, 1, m, plik);
     bzero(buffer, SIZE);
   }
+  fclose(plik);
   return;
 }
 
@@ -43,13 +50,17 @@ int main(){
   int n;
   int socket_serv;
   int socket_client;
-  int new_socket;
-
-  
+  FILE *plik_log;
   struct sockaddr_in server_addr, new_addr;
   socklen_t addr_size;
+  plik_log = fopen("plik_log.txt", "a");
   
   signal (SIGINT, handle_ctrlc);
+  
+  if (plik_log == NULL){
+    printf("BLAD otwierania pliku z logami.\n");
+    exit(1);
+  }
   
   socket_serv = socket(AF_INET, SOCK_STREAM, 0);
   if (socket_serv<0){
@@ -81,7 +92,8 @@ int main(){
   
   while(1){
     printf("Server oczekuje...\n");
-    socket_client = accept(socket_serv, (struct sockaddr*) NULL, NULL);
+    socket_client = accept(socket_serv, (struct sockaddr*)&new_addr, &addr_size);
+    addr_size = sizeof(new_addr);
 		
     if (socket_client<0){
     printf("BLAD SOCKETU SERWERA.");
@@ -89,11 +101,13 @@ int main(){
     }
 	  
     pid = fork();
+
     if(pid == 0){
         printf("Nawiazano polaczenie z klientem.\n");
-        addr_size = sizeof(new_addr);
- 	new_socket = accept(socket_serv, (struct sockaddr*)&new_addr, &addr_size);
-  	write_file(new_socket);
+        
+        fprintf(plik_log, "Polaczony klient: IP=%s, Port=%d, Plik:%s\n", ip, port, "write.txt");
+        
+  	write_file(socket_client);
   	printf("Pomyslnie zapisano plik.\n");
 	
         if(close(socket_client) == 0){
@@ -104,5 +118,6 @@ int main(){
         break;
     }
   }
+  fclose(plik_log);
   return 0;
 }
